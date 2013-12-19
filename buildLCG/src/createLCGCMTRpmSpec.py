03ef6c1d5b959779e0a5e02deabb594b5af73810
Ben Couturier <ben.couturier@cern.ch>
2013-12-19 16:23:52 +0100

    First fixes for the new format LCGCMT data

--- buildLCG/src/createLCGCMTRpmSpec.py
+++ buildLCG/src/createLCGCMTRpmSpec.py
@@ -8,21 +8,31 @@ rpm -bb <specfile>
 
 
 """
-import json
+import copy
+import logging
 import os
+import re
 import sys
 from string import Template
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+CONTAINER_LIST = [ "pytools", "pygraphics", "pyanalysis"]
+LCGNAME = "LCG"
+IGNORE_PKG = [ "yoda", "herwig++" ]
 
 # Class representing a RPM
 ###############################################################################
 class Rpm(object):
     """ Base class representing a RPM used to generate all the data related """
-    def __init__(self, lcgcmt_version, name, version, config, hat = None):
+    def __init__(self, lcgcmt_version, name, version, config, dirname, hat = None):
         self.lcgcmt_version = lcgcmt_version
         self.name = name
         self.version = version
         self.config = config
         self.hat = hat
+        self.dirname = dirname
         self.deps = list()
         self.dict = None
 
@@ -32,14 +42,13 @@ class Rpm(object):
             pre = self.hat + "_" + self.name
 
         post = (self.version + "_" + self.config).replace("-", "_")
-        return "LCGCMT_" + self.lcgcmt_version + "_" + pre + "_" + post
+        return LCGNAME + "-" + self.lcgcmt_version + "_" + pre + "_" + post
 
     def getRPMVersion(self):
         return "1.0.0"
 
     def getDiskPath(self):
-        return os.path.join("LCGCMT_" + self.lcgcmt_version, self.name, self.version, self.config)
-
+        return os.path.join(LCGNAME +"-" + self.lcgcmt_version, self.dirname , self.version, self.config)
 
     def __str__(self):
         strg = "RPM: %s-%s\n" % (self.getRPMName(), self.getRPMVersion())
@@ -75,48 +84,119 @@ AutoReqProv: no
             rpm_packages += "Requires: %s\n" % drpm.getRPMName()
         return rpm_packages
 
-
-
 # Directory configuration
 ###############################################################################
 def loadLCGCMTJSON(filename):
     """ Load the JSON file and check the basic structure """
     # First loading the JSON
-    with open(filename) as json_file:
-    #    data = json.load(json_file)
-        data = eval(json_file.read())
+    with open(filename) as inputfile:
+        inputdata = inputfile.readlines()
 
-    # Now checking that it has at least
-    required = ['packages', 'description']
-    for r in required:
-        if r not in data.keys():
-            raise Exception("Missing entry %s in JSON file", r)
+    packages = {}
+    for line in inputdata:
+        # Parse the file
+        # Ignore commments
+        if re.match("^\s*#", line):
+            logging.debug("Found comment: %s" % line)
+            continue
+        # Look for specific compiler line
+        if re.match("^\s*COMPILER", line):
+            logging.debug("Found COMPILER: %s" % line)
+            continue
+        # Parse proper line
+        # name-hash; package name; version; hash; full directory name; comma separated dependencies
+        (namehash, name, version, hash, dirname, deps) = line.split(";")
+        #log.debug((namehash, name, version, hash, dirname, deps))
+        pdata = {}
+        pdata['name'] = name
+        pdata['namehash'] = namehash
+        pdata['hash'] = hash
+        pdata['version'] = version
+        pdata['dirname'] = dirname
+        pdata['dependencies'] = [ e for e in deps.strip().split(",") if e != "" ]
+        packages[namehash] = pdata
+        log.debug(pdata)
+        
+    data = {}
+    data['packages'] = packages
 
-    version = data['description']['version']
-    platform = data['description']['platform']
+    return  data
 
-    if version == None or platform == None:
-        raise Exception("Missing data in JSON: version:%s, Platform: %s" % (version, platform))
+# Remove container and clean dependencies
+###############################################################################
+def fixLCGCMTData(data):
+    """ Convert the file to something usebale by LCGCMT users """
 
-    return (version, platform, data)
+    packages = data["packages"]
 
-def prepareRPMDict(data):
+    # Check which directories are used several times
+    # usecount = dict()
+    # for k in packages.values():
+    #     dirname = k['dirname']
+    #     usecount[dirname] = usecount.get(dirname, 0) + 1
+    # for k in  usecount.keys():
+    #     print "%s   -> %d" % (k, usecount[k])
+    # containers = [ k for k in usecount.keys() if usecount[k] > 1 ]
+    # print containers
+
+    newpackages = dict()
+    for pack in packages.values():
+        name = pack["name"]
+        version = pack["version"]
+        dirname = pack["dirname"]
+
+        # Ignoring packages contained in a container
+        if (name != dirname and dirname in CONTAINER_LIST) \
+               or name in IGNORE_PKG:
+            log.debug("Ignoring package " + name + " " + version + " in " + dirname)
+            continue
+        
+        # Now filtering the dependencies
+        log.debug("Filtering deps for " + name + " " + version + " in " + dirname)
+        deps =  pack["dependencies"]
+        newdeps = []
+        for d in deps:
+            log.debug("Identifying dependency: %s" % d)
+            dpack = packages[d]
+            if dpack['dirname'] in CONTAINER_LIST:
+                continue
+            if len([ip for ip in IGNORE_PKG if d.startswith(ip)]) > 0:
+                continue
+            newdeps.append(d)
+
+        # Copying and fixing deps
+        newpack = copy.deepcopy(pack)
+        newpack['dependencies'] = newdeps
+        newpackages[newpack['namehash']] = newpack
+
+    newdata = dict()
+    newdata['packages'] = newpackages
+    log.debug("Finished fixing the dependencies")
+    return newdata
+        
+def prepareRPMDict(data, lcgcmt_version, platform):
     """ Extract a list of RPM Objects based on the LCGCMT packages """
-    lcgcmt_version = data["description"]["version"]
-    platform = data["description"]["platform"]
     rpmDict= dict()
     packagesMap = data['packages']
     for k, d in packagesMap.items():
-        name = d["dest_name"]
+        name = d["name"]
         version = d["version"]
+        dirname = d["dirname"]
         if not name:
-            print >> sys.stderr,  "Missing name for key: %s" % k
+            log.error("Missing name for key: %s" % k)
             name = k
-        print >> sys.stderr, "Adding package " + name + " " + version
-        r = Rpm(lcgcmt_version, name, version , platform)
+        # Doublechecking...
+        deptype = "NORMAL"
+        if name != dirname \
+           and dirname in CONTAINER_LIST:
+            #log.debug("Ignoring package " + name + " " + version + " in " + dirname)
+            deptype = "SUBDEP"
+        log.debug("Adding package " + name + " " + version + " type:" + deptype)
+        r = Rpm(lcgcmt_version, name, version , platform, dirname)
         r.dependencies = list(d["dependencies"])
         r.dict = rpmDict
         rpmDict[k] = r
+    log.debug("All packages added")
     return rpmDict
 
 # Class to build the SPEC itself
@@ -124,17 +204,21 @@ def prepareRPMDict(data):
 
 class RpmSpec(object):
 
-    def __init__(self, jsonFile):
+    def __init__(self, version, platform, filename):
         """ Initialize with the list of RPMs """
-        (self.lcgcmt_version, self.lcgcmt_cmtconfig, self.data) = loadLCGCMTJSON(jsonFile)
-        self.rpmDict = prepareRPMDict(self.data)
+        self.lcgcmt_version = version
+        self.lcgcmt_cmtconfig = platform
+        tmpdata = loadLCGCMTJSON(filename)
+        self.data = fixLCGCMTData(tmpdata)
+        self.rpmDict = prepareRPMDict(self.data, self.lcgcmt_version,
+                                      self.lcgcmt_cmtconfig)
         self.rpmList = self.rpmDict.values()
 
         #topdir = "/scratch/z5/rpmbuild"
         #tmpdir = "/scratch/z5/tmpbuild"
         #rpmtmp = "/scratch/z5/tmp"
 
-        myroot = "/tmp"
+        myroot = "/home/opt/build"
         self.topdir = "%s/rpmbuild" % myroot
         self.tmpdir = "%s/tmpbuild" % myroot
         self.rpmtmp = "%s/tmp" % myroot
@@ -147,7 +231,6 @@ class RpmSpec(object):
         for d in [self.srcdir, self.rpmsdir, self.srpmsdir, self.builddir]:
             if not os.path.exists(d):
                 os.makedirs(d)
-
 
         self.buildroot = os.path.join(self.tmpdir, "LCGCMT-%s-%s-buildroot" % (self.lcgcmt_version, self.lcgcmt_cmtconfig))
         if not os.path.exists(self.buildroot):
@@ -250,7 +333,7 @@ cd ${RPM_BUILD_ROOT}/opt/lhcb/lcg
         for r in self.rpmList:
             path = r.getDiskPath()
             rpm_common += "mkdir -p ${RPM_BUILD_ROOT}/%s/%s\n" % (self.getLCGPath(),  path)
-            rpm_common += "rsync -ar %%{LCGCMTROOT}/%s/%s/%s/* ${RPM_BUILD_ROOT}%s/%s\n" % (r.name, r.version, r.config, self.getLCGPath(), path)
+            rpm_common += "rsync -ar %%{LCGCMTROOT}/%s/* ${RPM_BUILD_ROOT}%s/%s\n" % (path, self.getLCGPath(), path)
 
         rpm_common += """
 
@@ -310,8 +393,11 @@ cd ${RPM_BUILD_ROOT}/opt/lhcb/lcg
 
 
 if __name__ == '__main__':
-    #json_filename = "/afs/cern.ch/user/h/hegner/public/dependencies.json"
-    json_filename = "dependencies.json"
-    spec = RpmSpec(json_filename)
+    logging.basicConfig(stream=sys.stderr)
+    version = sys.argv[1]
+    platform = sys.argv[2]
+    input_filename = sys.argv[3]
+    log.debug("processing %s" % input_filename)
+    spec = RpmSpec(version, platform, input_filename)
     print spec.getSpec()
 
